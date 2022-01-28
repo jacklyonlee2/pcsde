Fixed data normalization bug, tested hyperparamter for fixed DSM

dataset.py
@@ -36,7 +36,7 @@ class ShapeNet15k(torch.utils.data.Dataset):
         if recenter:
             mx = np.amax(self.data, axis=1).reshape(B, 1, C)
             mn = np.amin(self.data, axis=1).reshape(B, 1, C)
-            self.mu = (mx + mx) / 2
+            self.mu = (mx + mn) / 2
             self.std = np.amax((mx - mn), axis=-1).reshape(B, 1, 1) / 2
         else:
             self.mu = self.data.reshape(-1, C).mean(axis=0).reshape(1, 1, C)

models/networks.py
@@ -52,7 +52,7 @@ class SetTransformer(nn.Module):
         self,
         dim_input=4,
         dim_ouput=3,
-        num_inds=64,
+        num_inds=128,
         dim_hidden=128,
         num_heads=4,
         ln=True,

trainers/base.py
@@ -121,7 +121,7 @@ class Trainer:
             self.epoch += 1
 
     def _test_step(self, x, mu, std):
-        prior = (torch.rand_like(x) * 2 - 1) * 1.5
+        prior = torch.rand_like(x) * 2 - 1
         o = self.sample(self.net, prior)
         # x, o = x * std + mu, o * std + mu  # denormalize
         return o, x

trainers/samplers.py
@@ -7,12 +7,14 @@ def langevin_dynamics(net, x, n_steps, step_lr, label=None):
     for t in range(n_steps):
         condx = torch.cat((x, label), dim=-1) if label else x
         noise = torch.randn_like(x) * np.sqrt(step_lr * 2)
-        x = x + step_lr * net(condx) + noise
+        x = x + noise
+        score = net(condx)
+        x = x + step_lr * score
     return x
 
 
 class LangevinSampler(nn.Module):
-    def __init__(self, n_steps=100, step_lr=1e-1):
+    def __init__(self, n_steps=200, step_lr=5e-4):
         super().__init__()
         self.n_steps = n_steps
         self.step_lr = step_lr
@@ -28,7 +30,7 @@ class LangevinSampler(nn.Module):
 
 
 class AnnealedLangevinSampler(nn.Module):
-    def __init__(self, sigmas, n_steps_each=10, step_lr=1e-1):
+    def __init__(self, sigmas, n_steps_each=20, step_lr=5e-4):
         self.sigmas = sigmas
         self.n_steps_each = n_steps_each
         self.step_lr = step_lr